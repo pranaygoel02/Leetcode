91378022e862bf68cf1788819e2358a0a7a5448d
Pranay Goel <goel.2002pranay@gmail.com>
2023-11-03 19:30:15 +0530

    Time: 15 ms (20.89%) | Memory: 13.8 MB (39.29%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,22 +4,34 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        vector<int> rows(n,0);
-        vector<int> cols(m,0);
-        
+        int col0 = 0;
+        int row0 = 0;
+
         for(int i = 0; i < matrix.size(); i++) {
+                        cout << i;
             for(int j = 0; j < matrix[0].size(); j++) {
                 if(matrix[i][j] == 0) {
-                    rows[i] = 1;
-                    cols[j] = 1;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                    if(i == 0) {
+                        col0 = 1;
+                    }
+                    if(j == 0) row0 = 1;
                 }
             }
         }
 
-        for(int i = 0; i < matrix.size(); i++) {
-            for(int j = 0; j < matrix[0].size(); j++) {
-                if(rows[i] == 1 || cols[j] == 1) matrix[i][j] = 0;
+        for(int i =  1 ; i < matrix.size(); i++) {
+            for(int j = 1; j < matrix[0].size(); j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
             }
+        }
+        cout << col0 << row0;
+        if(matrix[0][0] == 0) {
+            if(row0)
+                for(int i = 0; i < n; i++) matrix[i][0] = 0;
+            if(col0)
+                for(int i = 0; i < m; i++) matrix[0][i] = 0;
         }
     }
 };
