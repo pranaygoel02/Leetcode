185d2cf9178d26e3cd247ab25900c47da025251a
Pranay Goel <goel.2002pranay@gmail.com>
2023-11-14 19:42:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (25.99%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,20 +1,24 @@
 class Solution {
+    public int nCr(int n, int r) {
+        int ans = 1;
+        int diff = n - r;
+        for(int i = 1; i <= r; i++) {
+            ans = ans * n--;
+            ans = ans / i;
+        }
+        return ans;
+    }
+    public List<Integer> generateRow(int row) {
+        List<Integer> tmp = new ArrayList<>();
+        for(int i = 1; i <= row; i++) {
+            tmp.add(nCr(row - 1,i - 1));
+        }    
+        return tmp;
+    }
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> pascal = new ArrayList<>();
-        List<Integer> tmp = new ArrayList<>();
-        tmp.add(1);
-        pascal.add(new ArrayList<>(tmp));
-        for(int i = 1; i < numRows; i++) {
-            for(int j = 0; j <= i; j++) {
-                int sum = (j >= i ? 0 : pascal.get(i-1).get(j)) + (j > 0 ? pascal.get(i-1).get(j-1) : 0);
-                if(tmp.size() == j) {
-                    tmp.add(sum);
-                }
-                else {
-                    tmp.set(j,sum);
-                }
-            }
-            pascal.add(new ArrayList<>(tmp));
+        for(int i = 1; i <= numRows; i++) {
+            pascal.add(generateRow(i));
         }
         return pascal;
     }
