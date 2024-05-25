1f79a20fbc7cc4781986d6455ca9200466f01d27
Pranay Goel <goel.2002pranay@gmail.com>
2024-05-25 17:38:33 +0530

    Time: 9 ms (100.00%) | Memory: 40.6 MB (13.13%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long power(long x, long y, int mod) {
+        long res = 1;
+        long _y = y;
+        y = Math.abs(y);
+        while(y > 0) {
+            if((y & 1) != 0) {
+                res = (res * x) % mod;
+            }
+            y = y >> 1;
+            x = (x * x) % mod;
+        }
+        System.out.println(res);
+        return res % mod;
+    }
+    public int countGoodNumbers(long n) {
+        int mod = (int)(1e9) + 7;
+        long o = power(5,(n+1)/2, mod);
+        long e = power(4,n/2, mod);
+        System.out.printf("%d %d", o, e);
+        return (int)((e * o) % mod);
+    }
+}
